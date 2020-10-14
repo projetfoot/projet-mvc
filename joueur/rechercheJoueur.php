ff99c4e412b9de7b1e3e11457446d01e278f392c
jean13011 <jean-13011@hotmail.fr>
2020-10-14 13:12:31 +0200

    changement pour le dossier lib

--- joueur/rechercheJoueur.php
+++ joueur/rechercheJoueur.php
@@ -1,14 +1,7 @@
 <?php 
-
     define('ROOT', __DIR__);
     define('DS', DIRECTORY_SEPARATOR);
-
-    //creation des varibales filtrées
-    $id = filter_input(INPUT_POST,'id');
-    $pays = filter_input(INPUT_POST,'pays');
-    $nom = filter_input(INPUT_POST,'nom');
-    $prenom = filter_input(INPUT_POST,'prenom');
-    $date = filter_input(INPUT_POST,'date');
+    
 
     //varible pour la recherche 
     $snom = filter_input(INPUT_POST,'snom');
@@ -17,31 +10,25 @@
 
     
     //appel de notre fichier classPlayer
-    
     require_once (dirname(ROOT) . DS ."classes/classPlayer.php");
-    require_once (dirname(ROOT) . DS ."lib/functions.php");
+    require_once (dirname(ROOT) . DS ."lib/function.php");
 
     //etablissement d'une nouvelle connexion
     $dbh = new Connect;
 
     //si la page compte au moin un post la condition s'execute
     if(count($_POST) > 0){
-        if($_POST['sub'] == 1) {
-            //ajout des paramètres dans le constructeur
-            $bdd = new Player($pays,$nom,$prenom,$date);
-            //appel de la méthode
-            $bdd->write(); 
-        }
+        
 
         if($_POST['sub'] == 2){
             $poster = new Player(); 
-            
         } 
 
+        if($_POST["delete"] == 1 ){
+            $delete = new Player();
+        }
     }
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,20 +37,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>inscrivez un joueur dans la bdd.</h1>
-    <p> Si l'id entrée correspond a un joueur veillez réentrer tout les champs avec le champs que vous voulez changer </p>
-    <form  method='post'>
-        <label for="pays">pays</label>
-        <input type="text" name="pays" >
-        <label for="nom">nom</label>
-        <input type="text" name="nom" >
-        <label for="prenom">prenom</label>
-        <input type="text" name="prenom"> 
-        <label for="date">date de naissance</label>
-        <input type="date" name="date"> 
-        <button type="submit" name="sub" value="1">envoyer</button>
-    </form>
-
+    
     <h2>recherchez un joueur</h2>
     <form  method="post">
         <label for="nom">nom</label>
@@ -78,6 +52,7 @@
              display($poster);
         ?>
     </form>
-    <a href="contrat.php">créer un contrat</a>
+    <br>
+    <a href="inscriptionUpdate.php">inscire ou modifier un joueur</a>
 </body>
 </html>
--- lib/function.php
+++ lib/function.php
@@ -10,7 +10,7 @@ function display($poster){
     if(count($_POST) > 0 && $_POST['sub'] == 2){
         $req = $poster->poster( $_POST["snom"],$_POST["sprenom"],$_POST["spays"]);
         foreach($req as $key => $value){
-           echo  "<p> <strong>nom: </strong>" . $value["NOM_JOUEUR"] . "<br>" . "<strong>prenom: </strong> " . $value["PRENOM_JOUEUR"] . "<br>" . "<strong>pays: </strong> " . $value["ACRO_PAYS"] .  "</p>";
+           echo  "<p> <strong>nom: </strong>" . $value["NOM_JOUEUR"] . "<br>" . "<strong>prenom: </strong> " . $value["PRENOM_JOUEUR"] . "<br>" . "<strong>pays: </strong> " . $value["ACRO_PAYS"] .  "</p>" . "<button name='delete' value ='1' method='post'>supprimer</button>";
         }
         
     }
