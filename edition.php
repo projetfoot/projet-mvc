81c27a356f1e79485cd01bfc3e54ff21e82ce0e7
BenglerBrandon <brandon_du_13@hotmail.fr>
2020-10-15 14:31:19 +0200

    on peut  modifier ajouter et save un nouvel user

--- edition.php
+++ edition.php
@@ -1,31 +1,19 @@
 <?php
 
-try
-{   
-    //conection a MySQL
-$bdd = new PDO('mysql:host=localhost:3306;dbname=foot2;charset=utf8', 'root', '');
-}
-catch(exeption $e)
-{
-    //en cas d'erreur , on affiche un message et on arrete tout 
-    die('Erreur : ' . $e->getMessage());
-}
+require "user.php";
+$User =
+new User(array(
+    "id_user" =>"" ,
+    "nom_user" => 'francois',
+    "email_user"=> 'fg@hotmail.fr',
+    "niveau_de_droit"=> 15 ));
+    $User->save();
+    var_dump($User);
 
-
-//recupere le contenu de la table user
-$reponse = $bdd->query('SELECT * FROM user');
-
-//on affiche chaque entée 
-while ($donnees = $reponse->fetch())
-{
 ?>
-    <p>
-        <stong>USER</strong> :<?php echo $donnees['nom_user'];?> <br />
-        l'indentifiant est : <?php echo $donnees['id_user']; ?> <br />
-        email : <?php echo $donnees['email_user']; ?> <br />
-        niveau de droit : <?php echo $donnees['niveau_de_droit'] ; ?>
-    </p>
-    <?php
- }
 
- $reponse -> closeCursor();
+<form action="" method="post">
+<input type="text" name="auteur" value=""/>
+<input type="text" name="prix" value=""/>
+<input type="hidden" name="" value="'"/>
+<input type="submit"/>';
